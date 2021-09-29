1b76a8e6a0370735a9c458c44f6fec68a48e9b60
littlemole <littlemole@oha7.org>
2021-09-29 15:48:17 +0200

    boot

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,4 +1,10 @@
+@echo off
+if not exist vcpkg (
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
-bootstrap-vcpkg.bat
+cmd /C bootstrap-vcpkg.bat
 cd ..
+msbuild MTL.sln
+)
+set CMAKE_TOOLCHAIN_FILE=%~dp0vcpkg\scripts\toolchains\windows.cmake
+echo CMAKE_TOOLCHAIN_FILE=%CMAKE_TOOLCHAIN_FILE%
